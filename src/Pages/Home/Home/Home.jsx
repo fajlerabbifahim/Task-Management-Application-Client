b1bb8203644581fcc221823313f4b7b05c66738d
Yourfaahim <fahimqw1@gmail.com.com>
2025-02-21 22:00:13 +0600

    auth prover setup

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,13 +1,15 @@
 import TaskCard from "../../../Components/TaskCard/TaskCard";
+import useAuth from "../../../Hooks/useAuth";
 
 const Home = () => {
+  const { name } = useAuth();
   return (
     <div className="bg-[#333333] bg-gradient-to-b from-[#333333] to-[#6d6e6f] text-white p-4 h-full pt-40">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto  ">
-        <div className="border-2 border-red-500">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-6   ">
+        <div className="border-2 border-red-500 rounded-lg">
           <h1 className="text-center text-3xl pb-6 pt-6 border-b-2 ">TODO</h1>
           <TaskCard
-            title={"hello this is the task one Tanjila k chudbo"}
+            title={name}
             description={
               "Ajke tanzila K chudbo, bashai niye aisa. onek ador korbo voda chudbo, dhon dhukabo, dud chuso"
             }
--- src/Provider/AuthProvider/AuthProvider.jsx
+++ src/Provider/AuthProvider/AuthProvider.jsx
@@ -1,5 +1,28 @@
-const AuthProvider = () => {
-  return <div></div>;
+import { createContext, useEffect, useState } from "react";
+import auth from "../../firebase/firebase.config";
+import { onAuthStateChanged } from "firebase/auth";
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const name = "fahim";
+  const authInfo = { name };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => unSubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
