9d3e28502860322d0b3cbe6c7bedd70671d5cfa3
Yourfaahim <fahimqw1@gmail.com.com>
2025-02-22 15:57:26 +0600

    server link change

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://task-management-application-server-qx0spstqm.vercel.app",
 });
 const useAxiosPublic = () => {
   return axiosPublic;
